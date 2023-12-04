82d8ff7b0e60d5608e4668a6e0a905d70b921819
sangho-jeon <jeonsangho1587@gmail.com>
2023-12-04 22:13:30 +0900

    [feat] set to use fasthttpuser

--- Locust/locustfile.py
+++ Locust/locustfile.py
@@ -1,6 +1,11 @@
 from locust import HttpUser, task
 
-class ServerTest(HttpUser):
+class ServerTest(FastHttpUser):
     @task
     def api_test(self):
         self.client.get("/simplerequest")
+
+class MyLocust(Locust):
+ task_set = ServerTest
+ min_wait = 2000
+ max_wait = 5000
